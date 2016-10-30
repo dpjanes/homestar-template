8eec289206ce72329b26a2e97b6ae2c6a5e49815
David Janes <davidjanes@davidjanes.com>
2016-10-30 06:01:03 -0400

    ...

---
+++ models/template/index.js
@@ -0,0 +1,14 @@
+/*
+ *  index.js
+ *
+ *  David Janes
+ *  IOTDB
+ *  YYYY-MM-DD
+ */
+
+var iotdb = require("iotdb");
+
+exports.binding = {
+    bridge: require('../../TemplateBridge').Bridge,
+    model: require('./model.json'),
+};
